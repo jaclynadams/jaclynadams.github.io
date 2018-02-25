e0d0ba1d023972f1e5f460aaa8dc21c61d5905a9
Jaclyn Adams <jaadams05@gmail.com>
2018-02-25 15:48:40 -0800

    Restructure project and add dist folder

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Website coming soon!</h1>
+        <h1>Hello World</h1>
+      </div>);
+  }
+}
