29c95512152ab490e38b3958affc1f4a0add137b
Jaclyn Adams <jaadams05@gmail.com>
2018-02-25 15:48:09 -0800

    Remove gulp and add webpack sass-to-css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,22 @@
     ./webpack.config.js
 */
 const path = require('path');
+
 module.exports = {
-  entry: './client/index.js', // Need to update to correct entry point
+  entry: ['./src/index.js'], // './styles/sass/config.scss'], // Need to update to correct entry point
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
   },
+  watch: true,
+  devServer: {
+    inline: true
+  },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader']},
     ]
   }
 }
